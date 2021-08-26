fb3ad48af9756b3fbe2a5a4240742ea24c7a7cab
Invidam <hansupark0208@gmail.com>
2021-08-26 23:43:27 +0900

    Page Design Setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <article>
+        <h1>Hamburger App</h1>
+        <ol>
+          <li>Wake Up at 08:00 </li>
+          <li>Clean My room 3h </li>
+          <li>Do My homework 3h 20m </li>
+          <li>Test comment 2h 1m </li>
+          <li>Sleep at 01:00 </li>
+        </ol>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </span>
+      </article>
+    </main>
   );
 }
 
