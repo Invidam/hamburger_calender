712c5eff0df19e872a7206a0d84a8043dc83dc6e
Invidam <hansupark0208@gmail.com>
2021-11-19 15:22:13 +0900

    client build

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
 import "dotenv/config";
 // import { workListRouter } from "./routes/workList/workListRouter.js";
 import { authRouter } from "./routes/authRouter.js";
@@ -9,6 +10,7 @@ import { workListRouter } from "./routes/workListRouter.js";
 import { listViewRouter } from "./routes/listViewRouter.js";
 import { todoListRouter } from "./routes/todoListRouter.js";
 import { swaggerUi, specs } from "../modules/swagger.js";
+const __dirname = path.resolve();
 const port = 3002;
 const app = express();
 const logger = morgan("dev");
@@ -29,4 +31,8 @@ app.use("/api/:user/todolist", todoListRouter);
 // app.use("/api", apiRouter);
 app.use("/auth", authRouter);
 app.use("/api/:user/listview", listViewRouter);
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "build/index.html"));
+});
 app.listen(port, () => console.log(`Listening Start on port ${port}`));
--- src/setProxy.js
+++ src/setProxy.js
@@ -1,6 +1,15 @@
 import proxy from "http-proxy-middleware";
 
 export const proxyTest = (app) => {
-  app.use(proxy("/api", { target: "http://localhost:3002/" }));
-  app.use(proxy("/auth", { target: "http://localhost:3002/" }));
+  // app.use(
+  //   createProxyMiddleware("/api/data", {
+  //     target: "http://localhost:5000",
+  //   })
+  // );
+  app.use(
+    proxy("/api", { target: "http://localhost:3002/", changeOrigin: true })
+  );
+  app.use(
+    proxy("/auth", { target: "http://localhost:3002/", changeOrigin: true })
+  );
 };
