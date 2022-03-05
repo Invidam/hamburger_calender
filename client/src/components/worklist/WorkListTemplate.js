14aa539ba9570cbd5b7cf68bab097b2a93a43095
Invidam <hansupark0208@gmail.com>
2022-03-05 16:58:28 +0900

    feat: remove worklist's cog element.

--- client/src/components/worklist/WorkListTemplate.js
+++ client/src/components/worklist/WorkListTemplate.js
@@ -3,16 +3,11 @@ import { WorkList } from "./WorkList";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const CogElement = (
-  <FontAwesomeIcon className="workList-icon" icon={faCog} size="2x" />
-);
 export const WorkListTemplate = ({ targetSetting, user, date }) => {
   return (
     <div className="workList article-template">
       <header className="workList-header article-header">
         <h1>Make Hamburger</h1>
-
-        {CogElement}
       </header>
       <WorkList user={user} date={date} targetSetting={targetSetting} />
     </div>
